9d42e2c87a78cc21fd6adea5d1602ced8a899cbd
sryklief <sryklief@users.noreply.github.com>
2020-06-14 23:45:12 +0200

    .

---
+++ src/context/types.js
@@ -0,0 +1,7 @@
+export const SEARCH_USERS = 'SEARCH USERS';
+export const GET_USER = 'GET USER';
+export const CLEAR_USERS = 'CLEAR USERS';
+export const GET_REPOS = 'GET REPOS';
+export const SET_LOADING = 'SET LOADING';
+export const SET_ALERT = 'SET ALERT';
+export const REMOVE_ALERT = 'REMOVE ALERT';
